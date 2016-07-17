1a7dcb69b67dd73e75d5bbdfa087228d1575106f
shivamswarnkar <ss8464@nyu.edu>
2016-07-18 02:16:32 +0530

    added animation

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function animateName(id, initial_Size){
+	var elem = document.getElementBYId(id).style;
+	var size = elem.fontSize.slice(0, elem.fontSize.length-3);
+	var mult = .5;
+	if(size <= initial_Size ){
+
+	}
+	else{
+		
+	}
+
+
+}
